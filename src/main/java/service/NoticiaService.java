6e540deda60e04c00b38e95a833e02d34e238284
Leonardo Goandete <leonardo.goandete@hotmail.com>
2024-04-13 15:59:10 -0300

    Ajustes na classe NoticiaService

--- src/main/java/service/NoticiaService.java
+++ src/main/java/service/NoticiaService.java
@@ -1,13 +1,11 @@
 package service;
 
+import jakarta.enterprise.context.ApplicationScoped;
+import model.Noticia;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-
-import com.microsoft.playwright.*;
-import jakarta.enterprise.context.ApplicationScoped;
-import model.Noticia;
 
 import java.io.IOException;
 import java.net.URI;
@@ -18,104 +16,61 @@ import java.net.http.HttpResponse;
 import java.util.ArrayList;
 import java.util.List;
 
-//@ApplicationScoped
-//public class NoticiaService {
-//        public List<Noticia> obterNoticias() {
-//            List<Noticia> noticias = new ArrayList<>();
-//            try (Playwright playwright = Playwright.create()) {
-//                // Inicia o navegador
-//                Browser browser = playwright.firefox().launch();
-//                // Abre uma nova página
-//                Page page = browser.newPage();
-//
-//                // Realiza a solicitação POST para obter a página desejada
-//                String url = "https://poa.ifrs.edu.br/index.php/ultimas-noticias/noticias-principais";
-//                String postData = "limit=20"; // Se houver dados a serem enviados no corpo da solicitação POST, adicione aqui
-//                String contentType = "application/x-www-form-urlencoded";
-//
-//                HttpRequest request = HttpRequest.newBuilder()
-//                        .uri(new URI(url))
-//                        .header("Content-Type", contentType)
-//                        .POST(HttpRequest.BodyPublishers.ofString(postData))
-//                        .build();
-//
-//                HttpClient httpClient = HttpClient.newHttpClient();
-//                HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
-//
-//                // Carrega o conteúdo HTML da página
-//                String htmlContent = response.body();
-//                page.setContent(htmlContent);
-//
-//                // Realize a extração de dados da página como antes
-//                List<ElementHandle> listaNoticias = page.querySelectorAll("div.tileItem");
-//                for (ElementHandle noticia : listaNoticias) {
-//                    // Extrai o título e o resumo da notícia
-//                    String titulo = noticia.querySelector("h2.tileHeadline > a").innerText().trim();
-//                    String linkNoticia = noticia.querySelector("h2.tileHeadline > a").getAttribute("href");
-//                    String resumo = noticia.querySelector("span.description > p").innerText().trim();
-//
-//                    // Extrai a data e a hora da publicação
-//                    String dataPublicacao = noticia.querySelector("div.span2.tileInfo > ul > li:nth-child(3)").innerText().trim();
-//                    String horaPublicacao = noticia.querySelector("div.span2.tileInfo > ul > li:nth-child(4)").innerText().trim();
-//
-//                    noticias.add(
-//                            new Noticia(linkNoticia,
-//                                    titulo,
-//                                    resumo,
-//                                    dataPublicacao,
-//                                    horaPublicacao));
-//                }
-//
-//                browser.close();
-//            } catch (Exception e) {
-//                e.printStackTrace();
-//            }
-//            return noticias;
-//        }
-
 @ApplicationScoped
 public class NoticiaService {
     public List<Noticia> obterNoticias(int limit, String filterSearch) {
         List<Noticia> noticias = new ArrayList<>();
+        String url = "https://poa.ifrs.edu.br/index.php/ultimas-noticias/noticias-principais";
+        String postData = configuraFiltroNoticia(limit,filterSearch); // Se houver dados a serem enviados no corpo da solicitação POST
+        String contentType = "application/x-www-form-urlencoded";
+
         try {
             // Realiza a solicitação POST para obter o conteúdo da página desejada
-            String url = "https://poa.ifrs.edu.br/index.php/ultimas-noticias/noticias-principais";
-            String postData = configuraFiltroNoticia(limit,filterSearch); // Se houver dados a serem enviados no corpo da solicitação POST, adicione aqui
-            String contentType = "application/x-www-form-urlencoded";
+            HttpResponse<String> response = configuraConexao(url, contentType, postData);
+            // Realiza o scraping das informações HTML da página
+            scrapingInformacoesHtml(response, noticias);
 
-            HttpClient httpClient = HttpClient.newHttpClient();
+        } catch (IOException | InterruptedException | URISyntaxException e) {
+            System.out.println("Erro ao obter as notícias." + e.getMessage());
+        }
+        return noticias;
+    }
+
+    private static HttpResponse<String> configuraConexao(String url, String contentType, String postData) throws URISyntaxException, IOException, InterruptedException {
+        HttpResponse<String> response;
+        try (HttpClient httpClient = HttpClient.newHttpClient()) {
             HttpRequest request = HttpRequest.newBuilder()
                     .uri(new URI(url))
                     .header("Content-Type", contentType)
                     .POST(HttpRequest.BodyPublishers.ofString(postData))
                     .build();
 
-            HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
-
-            // Carrega o conteúdo HTML da página
-            String htmlContent = response.body();
-
-            // Parseia o HTML usando Jsoup
-            Document document = Jsoup.parse(htmlContent);
-
-            // Encontra todas as notícias na página
-            Elements listaNoticias = document.select("div.tileItem");
-            for (Element noticia : listaNoticias) {
-                // Extrai o título e o resumo da notícia
-                String titulo = noticia.select("h2.tileHeadline > a").text().trim();
-                String linkNoticia = noticia.select("h2.tileHeadline > a").attr("href");
-                String resumo = noticia.select("span.description > p").text().trim();
-
-                // Extrai a data e a hora da publicação
-                String dataPublicacao = noticia.select("div.span2.tileInfo > ul > li:nth-child(3)").text().trim();
-                String horaPublicacao = noticia.select("div.span2.tileInfo > ul > li:nth-child(4)").text().trim();
-
-                noticias.add(new Noticia(linkNoticia, titulo, resumo, dataPublicacao, horaPublicacao));
-            }
-        } catch (IOException | InterruptedException | URISyntaxException e) {
-            e.printStackTrace();
+            response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
         }
-        return noticias;
+        return response;
+    }
+
+    private static void scrapingInformacoesHtml(HttpResponse<String> response, List<Noticia> noticias) {
+        // Carrega o conteúdo HTML da página
+        String htmlContent = response.body();
+
+        // Parseia o HTML usando Jsoup
+        Document document = Jsoup.parse(htmlContent);
+
+        // Encontra todas as notícias na página
+        Elements listaNoticias = document.select("div.tileItem");
+        for (Element noticia : listaNoticias) {
+            // Extrai o título e o resumo da notícia
+            String titulo = noticia.select("h2.tileHeadline > a").text().trim();
+            String linkNoticia = noticia.select("h2.tileHeadline > a").attr("href");
+            String resumo = noticia.select("span.description > p").text().trim();
+
+            // Extrai a data e a hora da publicação
+            String dataPublicacao = noticia.select("div.span2.tileInfo > ul > li:nth-child(3)").text().trim();
+            String horaPublicacao = noticia.select("div.span2.tileInfo > ul > li:nth-child(4)").text().trim();
+
+            noticias.add(new Noticia(linkNoticia, titulo, resumo, dataPublicacao, horaPublicacao));
+        }
     }
 
     private String configuraFiltroNoticia(int limit, String filterSearch) {
