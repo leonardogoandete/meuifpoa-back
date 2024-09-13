a3be668b7ff81297e3f36822afbf08e7def6725a
leonardo.goandete <leonardo.goandete@hotmail.com>
2024-09-12 21:11:47 -0300

    Add threads para sincronizar

--- src/main/java/br/com/ifrs/backend/service/SyncService.java
+++ src/main/java/br/com/ifrs/backend/service/SyncService.java
@@ -7,6 +7,7 @@ import com.google.cloud.firestore.Firestore;
 import com.google.firebase.cloud.FirestoreClient;
 import jakarta.enterprise.context.ApplicationScoped;
 
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -20,7 +21,6 @@ public class SyncService {
     public SyncService() {
     }
 
-
     public void sincronizar(String uid, Login login) {
         String cpf = getCpfFromFirestore(uid);
         if (cpf == null) {
@@ -29,17 +29,27 @@ public class SyncService {
         }
 
         logger.info("Iniciando sincronização com o SIGAA para o CPF: " + cpf);
+
+        CompletableFuture<Void> notasFuture = CompletableFuture.runAsync(() -> {
             try {
-                //Obter notas
                 NotasService notasService = new NotasService();
                 notasService.obterNotas(uid, login.getSenha());
-                //Obter perfil
+            } catch (Exception e) {
+                logger.log(Level.SEVERE, "Erro ao obter notas", e);
+            }
+        });
+
+        CompletableFuture<Void> perfilFuture = CompletableFuture.runAsync(() -> {
+            try {
                 PerfilService perfilService = new PerfilService();
                 perfilService.obterDadosUsuario(uid, login.getSenha());
-            }catch (Exception e) {
-                logger.log(Level.SEVERE, "Erro ao sincronizar com o SIGAA", e);
+            } catch (Exception e) {
+                logger.log(Level.SEVERE, "Erro ao obter perfil", e);
             }
+        });
 
+        CompletableFuture<Void> allOf = CompletableFuture.allOf(notasFuture, perfilFuture);
+        allOf.join(); // Isso bloqueia até que ambas as tarefas sejam concluídas
     }
 
     private String getCpfFromFirestore(String uid) {
