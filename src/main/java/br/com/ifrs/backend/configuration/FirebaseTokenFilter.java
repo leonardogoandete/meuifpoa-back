ffc32ece42877f7adea51b47ccd38774c76e737b
leonardo.goandete <leonardo.goandete@hotmail.com>
2024-09-25 13:50:40 -0300

    Add filter

---
+++ src/main/java/br/com/ifrs/backend/configuration/FirebaseTokenFilter.java
@@ -0,0 +1,55 @@
+//package br.com.ifrs.backend.configuration;
+//
+//import br.com.ifrs.backend.exception.UnauthorizedException;
+//import com.google.firebase.auth.FirebaseAuth;
+//import com.google.firebase.auth.FirebaseToken;
+//import jakarta.annotation.Priority;
+//import jakarta.enterprise.context.ApplicationScoped;
+//import jakarta.ws.rs.Priorities;
+//import jakarta.ws.rs.container.ContainerRequestContext;
+//import jakarta.ws.rs.container.ContainerRequestFilter;
+//import jakarta.ws.rs.ext.Provider;
+//
+//import java.util.logging.Logger;
+//
+//@Provider
+//@Priority(Priorities.AUTHENTICATION)
+//@ApplicationScoped
+//public class FirebaseTokenFilter implements ContainerRequestFilter {
+//
+//    private static final Logger logger = Logger.getLogger(FirebaseTokenFilter.class.getName());
+//
+//    @Override
+//    public void filter(ContainerRequestContext requestContext) {
+//        logger.info("Interceptando requisição para autenticação");
+//
+//        if (requestContext.getUriInfo().getPath().contains("noticias")) {
+//            logger.info("Endpoint liberado: " + requestContext.getUriInfo().getPath());
+//            return;
+//        }
+//
+//        String authHeader = requestContext.getHeaderString("Authorization");
+//        logger.info("Cabeçalho Authorization: " + authHeader);
+//
+//        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+//            throw new UnauthorizedException("Token JWT ausente ou inválido.");
+//        }
+//
+//        String idToken = authHeader.substring(7); // Remove "Bearer "
+//
+//        try {
+//            // Verifica o token do Firebase
+//            FirebaseToken decodedToken = FirebaseAuth.getInstance().verifyIdToken(idToken);
+//            String uid = decodedToken.getUid();
+//
+//            logger.info("Token válido para o UID: " + uid);
+//
+//            // Anexa o UID ao contexto de segurança
+//            requestContext.setSecurityContext(new FirebaseSecurityContext(uid));
+//
+//        } catch (Exception e) {
+//            logger.severe("Token JWT inválido: " + e.getMessage());
+//            throw new UnauthorizedException("Token JWT inválido: " + e.getMessage());
+//        }
+//    }
+//}
