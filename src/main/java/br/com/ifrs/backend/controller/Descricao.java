f98d6bfd9d14a018d9c72136144d88c84c6208a8
leonardo.goandete <leonardo.goandete@hotmail.com>
2024-09-30 23:17:44 -0300

    Add descrições swagger

--- src/main/java/br/com/ifrs/backend/controller/Descricao.java
+++ src/main/java/br/com/ifrs/backend/controller/Descricao.java
@@ -18,7 +18,7 @@ import org.eclipse.microprofile.openapi.annotations.tags.Tag;
                 )
         ),
         servers = {
-                @Server(url = "https://app.ifrs.edu.br/meuifpoa/"),
+                @Server(url = "https://app.ifrs.edu.br"),
                 @Server(url = "http://localhost:8080")
 
         }
