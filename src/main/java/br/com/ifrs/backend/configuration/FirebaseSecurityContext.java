97e9b43bdca6e388bfb7a3113ca8b9c35bfbade0
leonardo.goandete <leonardo.goandete@hotmail.com>
2024-10-07 12:14:52 -0300

    Add ajustes

--- src/main/java/br/com/ifrs/backend/configuration/FirebaseSecurityContext.java
+++ src/main/java/br/com/ifrs/backend/configuration/FirebaseSecurityContext.java
@@ -1,33 +1,33 @@
-//package br.com.ifrs.backend.configuration;
-//
-//import jakarta.ws.rs.core.SecurityContext;
-//import java.security.Principal;
-//
-//public class FirebaseSecurityContext implements SecurityContext {
-//
-//    private final String uid;
-//
-//    public FirebaseSecurityContext(String uid) {
-//        this.uid = uid;
-//    }
-//
-//    @Override
-//    public Principal getUserPrincipal() {
-//        return () -> uid;
-//    }
-//
-//    @Override
-//    public boolean isUserInRole(String role) {
-//        return false;
-//    }
-//
-//    @Override
-//    public boolean isSecure() {
-//        return true;
-//    }
-//
-//    @Override
-//    public String getAuthenticationScheme() {
-//        return "JWT";
-//    }
-//}
+package br.com.ifrs.backend.configuration;
+
+import jakarta.ws.rs.core.SecurityContext;
+import java.security.Principal;
+
+public class FirebaseSecurityContext implements SecurityContext {
+
+    private final String uid;
+
+    public FirebaseSecurityContext(String uid) {
+        this.uid = uid;
+    }
+
+    @Override
+    public Principal getUserPrincipal() {
+        return () -> uid;
+    }
+
+    @Override
+    public boolean isUserInRole(String role) {
+        return false;
+    }
+
+    @Override
+    public boolean isSecure() {
+        return true;
+    }
+
+    @Override
+    public String getAuthenticationScheme() {
+        return "JWT";
+    }
+}
