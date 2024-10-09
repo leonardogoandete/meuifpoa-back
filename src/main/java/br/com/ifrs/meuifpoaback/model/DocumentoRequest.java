42c94311fd5ab34c95e5a2ad87a9236635526554
leonardo.goandete <leonardo.goandete@hotmail.com>
2024-10-09 15:34:01 -0300

    Atualizado arquivos javadoc

--- src/main/java/br/com/ifrs/meuifpoaback/model/DocumentoRequest.java
+++ src/main/java/br/com/ifrs/meuifpoaback/model/DocumentoRequest.java
@@ -2,5 +2,7 @@ package br.com.ifrs.meuifpoaback.model;
 
 /**
  * A classe DocumentoRequest representa uma solicitação de documento.
+ * @param tipo o tipo de documento a ser solicitado
+ * @param senha a senha do usuário para o SIGAA
  */
 public record DocumentoRequest (String tipo, String senha) {}
